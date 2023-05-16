c42f160564b0e29c616445905274a29e27fe1b60
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2023-05-16 10:36:12 +0100

    fixed bug by removing module reference

--- ObjectiveC-UIKit/ObjectiveC-UIKit/View/View/ViewController.m
+++ ObjectiveC-UIKit/ObjectiveC-UIKit/View/View/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"Bullshit");
+    NSLog(@"Not Working");
 }
 
 - (IBAction)goToPersonPage:(id)sender {
